50af6386b09593bc5ab5937252e7eae671d88883
Ekuesky <ayiekue9127@gmail.com>
2024-09-16 16:10:54 +0000

    Implementing pagination

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -5,11 +5,14 @@ import { Suspense } from 'react';
 import {InvoicesTableSkeleton} from "@/app/ui/skeletons";
 import Pagination from "@/app/ui/invoices/pagination";
 import Table from "@/app/ui/invoices/table";
+import { fetchInvoicesPages } from "@/app/lib/data";
 
 export default async function Page({searchParams,
                                    }: { searchParams?: { query?: string; page?: string; }; }) {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
+
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -23,7 +26,7 @@ export default async function Page({searchParams,
                 <Table query={query} currentPage={currentPage}/>
             </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                 {/*<Pagination totalPages={totalPages} /> */}
+                 <Pagination totalPages={totalPages} />
             </div>
         </div>
     );
