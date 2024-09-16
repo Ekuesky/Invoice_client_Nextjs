f0b7590869c7c92979105f2cc16f48e81b12617c
Ekuesky <ayiekue9127@gmail.com>
2024-09-16 14:16:45 +0000

    feat: implement partial prerendering

--- nextjs-dashboard/next.config.mjs
+++ nextjs-dashboard/next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    // enable ppr option
+    experimental: {
+        ppr: 'incremental',
+    }
+};
 
 export default nextConfig;
