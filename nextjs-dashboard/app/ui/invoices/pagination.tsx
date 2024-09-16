4936c7d5ce37bb325ce46a9904c34dea87606371
Ekuesky <ayiekue9127@gmail.com>
2024-09-16 16:32:33 +0000

    bug:fixed on pagination.tsx

--- nextjs-dashboard/app/ui/invoices/pagination.tsx
+++ nextjs-dashboard/app/ui/invoices/pagination.tsx
@@ -8,10 +8,12 @@ import { usePathname, useSearchParams } from 'next/navigation'
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
 
-  const allPages = generatePagination(currentPage, totalPages);
+
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
+
+  const allPages = generatePagination(currentPage, totalPages);
 
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
