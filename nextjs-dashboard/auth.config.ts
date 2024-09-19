68aba3e397ac60386d42d4b75714b1b1f3037de7
Ekuesky <ayiekue9127@gmail.com>
2024-09-19 20:44:58 +0000

    fix: added use client to login form

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -1,21 +1,20 @@
 import type { NextAuthConfig } from 'next-auth';
 
-export const authConfig = {
+export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/login',
   },
-    callbacks: {
+  callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
+      if (nextUrl.pathname.startsWith('/dashboard')) {
+        // Redirect to login if not authenticated
+        return !!auth?.user;
+      } else if (auth?.user) {
+        // Redirect authenticated users to dashboard
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
-} satisfies NextAuthConfig;
+  providers: [],
+};
